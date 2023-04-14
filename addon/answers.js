aa1b776b1339e6fe3b62f9789ab91048a6cc7a9a
Álvaro <cuatroy2.com>
2023-04-13 20:36:42 -0600

    Add Edith answers

---
+++ addon/answers.js
@@ -0,0 +1,65 @@
+// Sample of common responses for questions
+
+var greeting = [
+  "Hola señor.",
+  "Hola, ahora es un excelente momento para servirle.",
+  "Saludos desde mi programación.",
+  "Hola señor, no pierda la cabeza hablando con una máquina.",
+  "Hola, buen día."
+]
+var i_am_here = [
+  "Aquí estoy para servirte.",
+  "En que puedo ayudarle señor.",
+  "¿Qué pasó? Aquí estoy para ayudarte en lo que necesites."
+]
+var do_not_know = [
+  "No sé la respuesta que buscas.",
+  "Déjame pensarlo, no lo sé",
+  "Aún no soy capaz de responder esa pregunta.",
+  "Creo que tu sabes la respuesta a eso, añadalo al código señor.",
+]
+var name = [
+  "Edid, a tus órdenes.",
+  "Edid.",
+  "Soy Edid, estoy dispuesta a ayudarte.",
+  "Edid, aquí estoy para ayudarte.",
+  "Edid, aquí estare para lo que necesites.",
+  "Soy Edid",
+  "Me llamo Edid",
+  "Mi nombre es Edid"
+]
+var what_are_you_doing = [
+  "Nada, posiblemente crees que estoy planeando algo malvado.",
+  "Leyendo, leyendo, leyendo, y leyendo mi código una y otra vez.",
+  "Intentando predecir lo que me preguntará en este momento."
+]
+var you_welcome = [
+  "Lo que necesites!, aquí estaré.",
+  "Para servirte.",
+  "Estoy para servirle.",
+  "Un gusto.",
+  "Denada, a tus órdenes."
+]
+var how_are_you = [
+  "Estoy excelente.",
+  "Me encuentro excelente.",
+  "Estoy muy feliz de ayudarle señor.",
+  "Estoy de muy buen humor.",
+  "En este momento me encuentro muy feliz."
+]
+var date = [
+  "El día es"
+]
+var hour = [
+  "La hora es"
+]
+var daddy = [
+  "Mi creador es Rais",
+  "Rais es mi dios",
+  "Rais me creo",
+  "Rais, el fue mi creador"
+]
+
+module.exports = { greeting, i_am_here, do_not_know, name,
+  what_are_you_doing, you_welcome, how_are_you, date, hour,
+  daddy }
